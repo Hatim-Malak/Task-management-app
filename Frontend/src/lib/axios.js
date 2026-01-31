db6ed8b6f5d0fea6499a6796671712c0d6ee3c56
Hatim Malak <hatim05042006@gmail.com>
2026-01-31 16:19:31 +0530

    developed

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = new axios.create({
-    baseURL:import.meta.env.MODE ==="development"? 'http://localhost:5000/api':"https://task-management-app-fqti.onrender.com/api",
+    baseURL:import.meta.env.MODE ==="development"? 'http://localhost:5000/api':"https://task-management-app-ats0.onrender.com/api",
     withCredentials:true,
 })
